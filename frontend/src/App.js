ea9d1fe73511e62563cfc3209243d9cb6a7b52e3
CharithAnjana <charithanjana01@gmail.com>
2022-07-04 12:22:42 +0530

    Done SignUp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import React from "react";
 import "swiper/css/bundle";
 
 import HomePage from "./pages/HomePage/HomePage";
+import SignUpCreatorPage from "./pages/SignUpCreatorPage/SignUpCreatorPage";
+import SignUpCreatorPage2 from "./pages/SignUpCreatorPage/SignUpCreatorPage2";
+import SignUpFollowerPage from "./pages/SignUpFollowerPage/SignUpFollowerPage";
+import SignUpOptionPage from "./pages/SignUpOptionPage/SignUpOptionPage";
 
 
 function App() {
