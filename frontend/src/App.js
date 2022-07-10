7b4bd76bb82031547539067b4865367c46334231
CharithAnjana <charithanjana01@gmail.com>
2022-07-10 14:47:15 +0530

    done sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import "swiper/css/bundle";
 
+import FeedPage from "./pages/FeedPage/FeedPage";
 import HomePage from "./pages/HomePage/HomePage";
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>      
+      <FeedPage/>      
     </div>
   );
 }
---
+++ frontend/src/components/SideNavBar/SideNavBar.js
@@ -0,0 +1,51 @@
+import "./SideNavBar.css";
+
+
+function SideNavBar() {
+    return (
+        <>
+            <nav id="sidebar">
+                <div className="content">
+                    <ul class="list-unstyled">
+                        <li class="activesb">
+                            <a><i class="bi bi-rss icon-theme"></i>Feed</a>
+                        </li>
+                        <li>
+                            <a><i class="bi bi-people-fill icon-theme"></i>Find Creators</a>
+                        </li>
+                        <li>
+                            <a><i class="bi bi-star icon-theme"></i>Favourits</a>
+                        </li>
+                        <li>
+                            <a><i class="bi bi-chat-dots icon-theme"></i>Chat</a>
+                        </li>
+                        <li>
+                            <a><i class="bi bi-graph-up icon-theme"></i>Statics</a>
+                        </li>
+                        <li>
+                            <a><i class="bi bi-gear icon-theme"></i>Settings</a>
+                        </li>
+                        <li>
+                            <a><i class="bi bi-badge-ad-fill icon-theme"></i>Advertisment</a>
+                        </li>
+                    </ul><br />
+
+                    <div className="premium">
+                        <div className="content-pre">
+                            <p>
+                                Upgrade to premimum <i class="bi bi-gem icon-pre"></i>
+                            </p>
+                            <p>
+                                small description on to have
+                                interest on premium package
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default SideNavBar;
