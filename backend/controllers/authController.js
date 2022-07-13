a4cc6c881f9a70c12a8fa0cf0ea3986116803619
Janitha Devin Ratnayake <2019cs142@stu.ucsc.lk>
2022-07-13 14:59:36 +0530

    login and register api done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,62 @@
+const { PrismaClient } = require("@prisma/client");
 const { StatusCodes } = require("http-status-codes");
+const bycrypt = require("bcrypt");
 
-const register = async (req, res) => {};
+const { user } = new PrismaClient();
 
-module.exports = { register };
+const register = async (req, res) => {
+  const { type, firstName, lastName, email, accountStatus, password } =
+    req.body;
+
+  const emailStatus = await user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  //   res.json({ test: emailStatus });
+
+  //   return 0;
+  if (emailStatus) {
+    res.json({ error: "Email Already Registered! Please Login" });
+  } else {
+    bycrypt.hash(password, 10).then(async (hash) => {
+      const newUser = await user.create({
+        data: {
+          type,
+          firstName,
+          lastName,
+          email,
+          accountStatus,
+          password: hash,
+        },
+      });
+
+      res.status(StatusCodes.CREATED).json(newUser);
+    });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const existUser = await user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existUser) {
+    bycrypt.compare(password, existUser.password).then((match) => {
+      if (match) {
+        res.json(existUser);
+      } else {
+        res.json({ error: "Wrong Password Entered" });
+      }
+    });
+  } else {
+    res.json({ error: "Email Doesn't Exist" });
+  }
+};
+
+module.exports = { register, login };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 // import controllers
-const { register } = require("../controllers/authController");
+const { register, login } = require("../controllers/authController");
 
 const router = express.Router();
 
 // define specific routes
 router.route("/register").post(register);
+router.route("/login").post(login);
 
 module.exports = router;
