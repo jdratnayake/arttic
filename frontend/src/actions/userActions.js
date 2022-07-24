2d7dccf3ce921f378012ac101c459777de4180c2
Janitha Devin Ratnayake <2019cs142@stu.ucsc.lk>
2022-07-25 00:06:57 +0530

    redux initiated

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,5 @@
+import { USER_LOGIN, USER_LOGOUT } from "../constants/userConstants";
+
+export const login = (email, password) => async (dispatch) => {};
+
+export const logout = () => {};
---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,8 @@
+import { USER_LOGIN, USER_LOGOUT } from "../constants/userConstants";
+
+export const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
