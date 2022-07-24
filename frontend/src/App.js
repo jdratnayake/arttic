0857ee851cf79f78c8b86bfa015243d4b2d76348
Janitha Devin Ratnayake <2019cs142@stu.ucsc.lk>
2022-07-24 23:48:07 +0530

    part 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import SignUpOptionPage from "./pages/SignUpOptionPage/SignUpOptionPage";
 import SignInPage from "./pages/SIgnInPage/SignInPage";
 import FrogetPasswordPage1 from "./pages/FrogetPasswordPage/FrogetPasswordPage1";
 import FrogetPasswordPage2 from "./pages/FrogetPasswordPage/FrogetPasswordPage2";
-import ReportUserPage from "./pages/ReportUserPage/ReportUserPage"
+import ReportUserPage from "./pages/ReportUserPage/ReportUserPage";
 import Settings from "./components/Settings/Settings";
 import DashboardAdmin0Page from "./pages/DashboardAdmin0Page/DashboardAdmin0Page";
 import TransactionAdmin0Page from "./pages/TransactionAdmin0Page/TransactionAdmin0Page";
@@ -27,14 +27,20 @@ function App() {
         <Route path="/feed" element={<FeedPage />}></Route>
         <Route path="/settings" element={<Settings />}></Route>
         <Route path="/report" element={<ReportUserPage />}></Route>
-        <Route path="/login" element={<SignInPage />}></Route>
+        <Route path="/signupoption" element={<SignUpOptionPage />}></Route>
         <Route path="/signupfollower" element={<SignUpFollowerPage />}></Route>
         <Route path="/signupcreator" element={<SignUpCreatorPage />}></Route>
         <Route path="/signupcreator2" element={<SignUpCreatorPage2 />}></Route>
-        <Route path="/signupoption" element={<SignUpOptionPage />}></Route>
+        <Route path="/login" element={<SignInPage />}></Route>
         <Route path="/feed" element={<FeedPage />}></Route>
-        <Route path="/frogetpassword1" element={<FrogetPasswordPage1 />}></Route>
-        <Route path="/frogetpassword2" element={<FrogetPasswordPage2 />}></Route>
+        <Route
+          path="/frogetpassword1"
+          element={<FrogetPasswordPage1 />}
+        ></Route>
+        <Route
+          path="/frogetpassword2"
+          element={<FrogetPasswordPage2 />}
+        ></Route>
         <Route
           path="/admin0/dashboard"
           element={<DashboardAdmin0Page />}
--- frontend/src/components/SignUpOptionCard/SignUpOptionCard.js
+++ frontend/src/components/SignUpOptionCard/SignUpOptionCard.js
@@ -1,25 +1,36 @@
+import { useNavigate } from "react-router-dom";
+
 import "./SignUpOptionCard.css";
 
-function SignUpOptionCard(props) {
-    return (
-        <div class="card">
-            <div class="card-body">
-                <h4 class="title text-center theme">{props.title}</h4>
-                <p class="text-center">who is {props.av} - description</p>
-                <ul>
-                    <li class="listcd">{props.p1}</li>
-                    <li class="listcd">{props.p2}</li>
-                    <li class="listcd">{props.p3}</li>
-                    <li class="listcd">{props.p4}</li>
-                </ul>
+function SignUpOptionCard({ title, btn, av, p1, p2, p3, p4, link }) {
+  const navigate = useNavigate();
 
-                <div class="text-center">
-                    <button type="submit" class="btn btn-primary btn-signup">{props.btn}</button>
-                </div>
+  return (
+    <div class="card">
+      <div class="card-body">
+        <h4 class="title text-center theme">{title}</h4>
+        <p class="text-center">who is {av} - description</p>
+        <ul>
+          <li class="listcd">{p1}</li>
+          <li class="listcd">{p2}</li>
+          <li class="listcd">{p3}</li>
+          <li class="listcd">{p4}</li>
+        </ul>
 
-            </div>
+        <div class="text-center">
+          <button
+            type="submit"
+            class="btn btn-primary btn-signup"
+            onClick={() => {
+              navigate(link);
+            }}
+          >
+            {btn}
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default SignUpOptionCard;
