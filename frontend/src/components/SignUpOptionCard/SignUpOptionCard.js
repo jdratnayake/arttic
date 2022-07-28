41230e5779127975768399d980eb7bd5d0bea37a
Maleesha Gunarathna <MaleeshaGunarathna98@gmail.com>
2022-07-28 14:58:39 +0530

    signup option page and card chnages

--- frontend/src/components/SignUpOptionCard/SignUpOptionCard.js
+++ frontend/src/components/SignUpOptionCard/SignUpOptionCard.js
@@ -2,19 +2,20 @@ import { useNavigate } from "react-router-dom";
 
 import "./SignUpOptionCard.css";
 
-function SignUpOptionCard({ title, btn, av, p1, p2, p3, p4, link }) {
+function SignUpOptionCard({ title, btn, av, p1, p2, p3, p4,p5, link }) {
   const navigate = useNavigate();
 
   return (
     <div class="card">
       <div class="card-body">
-        <h4 class="title text-center theme">{title}</h4>
+        <h4 class="title text-center theme-title">{title}</h4>
         {/* <p class="text-center">who is {av} - description</p> */}
         <ul>
           <li class="listcd">{p1}</li>
           <li class="listcd">{p2}</li>
           <li class="listcd">{p3}</li>
           <li class="listcd">{p4}</li>
+          <li class="listcd">{p5}</li>
         </ul>
 
         <div class="text-center">
