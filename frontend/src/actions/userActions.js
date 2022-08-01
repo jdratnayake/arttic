fa94ee79812496d7ea25e45b9dbf3bcefa5e7705
Janitha Devin Ratnayake <2019cs142@stu.ucsc.lk>
2022-08-01 22:17:05 +0530

    Revert "Merge branch 'dev' into UI-touchup"
    This reverts commit a844c707d5882e2fb9a0ddef325f419280df6ac2, reversing
    changes made to 312413647b7c4dd2c06132a5b5ccf0c17c2b94dd.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -9,48 +9,31 @@ import {
 } from "../constants/userConstants";
 import { API_URL } from "../constants/globalConstants";
 
-export const login = (username, password) => async (dispatch) => {
-  try {
-    const inputData = { username, password };
-
-    await axios.post(API_URL + "/auth/login", inputData).then((response) => {
-      if (!response.data.error) {
-        dispatch({ type: USER_LOGIN_SUCCESS, payload: response.data });
-
-        localStorage.setItem("user", JSON.stringify(response.data));
-      } else {
-        dispatch({ type: USER_LOGIN_FAIL, payload: response.data });
-
-        localStorage.setItem("user", JSON.stringify(response.data));
-      }
-    });
-  } catch (error) {
-    dispatch({
-      type: USER_REGISTER_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
-};
+// export const login = (email, password) => async (dispatch) => {};
 
 // export const logout = () => {};
 
 export const register =
   (userType, name, email, password) => async (dispatch) => {
     try {
-      const inputData = { name, email, password, userType };
+      console.log("action");
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
 
-      await axios
-        .post(API_URL + "/auth/register", inputData)
-        .then((response) => {
-          if (!response.data.error) {
-            dispatch({ type: USER_REGISTER_SUCCESS, payload: response.data });
+      const { data } = await axios.post(
+        API_URL + "/api/v1/auth/register",
+        { name, email, password, userType },
+        config
+      );
 
-            localStorage.setItem("user", JSON.stringify(response.data));
-          }
-        });
+      //   dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+
+      dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+      localStorage.setItem("user", JSON.stringify(data));
     } catch (error) {
       dispatch({
         type: USER_REGISTER_FAIL,
--- frontend/src/constants/globalFunctions.js
+++
@@ -1,4 +0,0 @@
-export const getAccessToken = () => {
-  const { accessToken } = JSON.parse(localStorage.getItem("user"));
-  return accessToken;
-};
