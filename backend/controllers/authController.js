1856217a699b3f177267247cbd0ea8ac7ae5240f
Janitha Devin Ratnayake <2019cs142@stu.ucsc.lk>
2022-08-02 11:50:43 +0530

    backend bug fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,8 +2,24 @@ const { PrismaClient } = require("@prisma/client");
 const { StatusCodes } = require("http-status-codes");
 const bycrypt = require("bcrypt");
 const asyncHandler = require("express-async-handler");
+const { sign } = require("jsonwebtoken");
 
-const { user } = new PrismaClient();
+const { user, followerCreator, creator } = new PrismaClient();
+
+const emailCheck = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  const emailStatus = await user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (emailStatus) {
+    res.json({ isUnique: false });
+  } else {
+    res.json({ isUnique: true });
+  }
+});
 
 const register = asyncHandler(async (req, res) => {
   const { userType, name, email, password } = req.body;
@@ -14,13 +30,13 @@ const register = asyncHandler(async (req, res) => {
     },
   });
 
-  //   res.json({ test: emailStatus });
-
-  //   return 0;
+  // 3 = creator
+  // 4 = follower
   if (emailStatus) {
     res.json({ error: "Email Already Registered! Please Login" });
   } else {
     bycrypt.hash(password, 10).then(async (hash) => {
+      // add data to the user table
       const newUser = await user.create({
         data: {
           type: userType,
@@ -31,6 +47,28 @@ const register = asyncHandler(async (req, res) => {
         },
       });
 
+      // add data to the followerCreator table
+      const newFollower = await followerCreator.create({
+        data: {
+          userId: newUser.userId,
+        },
+      });
+
+      // add data to the creator table
+      if (userType === 3) {
+        const newCreator = await creator.create({
+          data: {
+            userId: newUser.userId,
+            pageName: newUser.username,
+          },
+        });
+      }
+
+      const accessToken = sign(
+        { email: newUser.email, userId: newUser.userId },
+        process.env.JWT_SECRET
+      );
+
       const returnData = {
         userId: newUser.userId,
         type: newUser.type,
@@ -39,6 +77,7 @@ const register = asyncHandler(async (req, res) => {
         emailStatus: newUser.emailStatus,
         username: newUser.username,
         profilePhoto: newUser.profilePhoto,
+        accessToken: accessToken,
       };
 
       res.status(StatusCodes.CREATED).json(returnData);
@@ -47,25 +86,186 @@ const register = asyncHandler(async (req, res) => {
 });
 
 const login = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
-  const existUser = await user.findUnique({
+  const existUser = await user.findFirst({
     where: {
-      email,
+      OR: [
+        {
+          email: username,
+        },
+        {
+          username: username,
+        },
+      ],
     },
   });
+
+  let creatorStatus;
+  if (existUser && existUser.type === 3) {
+    creatorStatus = await creator.findUnique({
+      where: {
+        userId: existUser.userId,
+      },
+      select: {
+        openSeaStatus: true,
+      },
+    });
+  }
+  // console.log(existUser);
 
   if (existUser) {
     bycrypt.compare(password, existUser.password).then((match) => {
       if (match) {
-        res.json(existUser);
+        const accessToken = sign(
+          { email: existUser.email, userId: existUser.userId },
+          process.env.JWT_SECRET
+        );
+
+        let returnData = {
+          userId: existUser.userId,
+          type: existUser.type,
+          name: existUser.name,
+          email: existUser.email,
+          emailStatus: existUser.emailStatus,
+          username: existUser.username,
+          profilePhoto: existUser.profilePhoto,
+          accessToken: accessToken,
+        };
+
+        if (existUser.type === 3) {
+          returnData = {
+            ...returnData,
+            openSeaStatus: creatorStatus.openSeaStatus,
+          };
+        }
+
+        res.json(returnData);
       } else {
-        res.json({ error: "Wrong Password Entered" });
+        res.json({
+          error: { username: "", password: "Wrong Password Entered" },
+        });
       }
     });
   } else {
-    res.json({ error: "Email Doesn't Exist" });
+    res.json({
+      error: { username: "Email or Username Doesn't Exist", password: "" },
+    });
   }
 });
 
-module.exports = { register, login };
+const creatorVerify = asyncHandler(async (req, res) => {
+  const pythonScript = "./scripts/account-extractor.py";
+
+  const uint8arrayToString = (data) => {
+    return String.fromCharCode.apply(null, data);
+  };
+
+  const { userId, openSeaUsername, walletAddress } = req.body;
+
+  const errorData = { username: "", walletAddress: "" };
+  let errorSignal = false;
+
+  const openSeaUsernameStatus = await creator.findUnique({
+    where: {
+      openSeaUsername,
+    },
+  });
+
+  if (openSeaUsernameStatus) {
+    errorData.username =
+      "OpenSea Username Already Registered! Please Enter Another One";
+
+    errorSignal = true;
+  }
+
+  const walletAddressStatus = await creator.findUnique({
+    where: {
+      walletAddress,
+    },
+  });
+
+  if (walletAddressStatus) {
+    errorData.walletAddress =
+      "Wallet Address Already Registered! Please Connect Another One";
+    errorSignal = true;
+  }
+
+  if (errorSignal) {
+    res.json({
+      error: errorData,
+    });
+    return 0;
+  }
+
+  const spawn = require("child_process").spawn;
+
+  const scriptExecution = spawn("python.exe", [pythonScript, openSeaUsername]);
+
+  // Handle normal output
+  scriptExecution.stdout.on("data", (data) => {
+    let profile = JSON.parse(uint8arrayToString(data)); // return a JSON object
+    // res.send(profile); // display in the browser
+    // console.log(profile);
+    accValidator(profile.account, walletAddress);
+  });
+
+  // Handle error output
+  scriptExecution.stderr.on("data", (data) => {
+    // As said before, convert the Uint8Array to a readable string.
+    console.log(uint8arrayToString(data));
+  });
+
+  let creatorStatus = 0;
+
+  // 1 = verified account
+  // 2 = valid account
+  // 3 = details mismatched
+  // 4 = unable to find the user account
+  const accValidator = async (account) => {
+    if (account !== undefined) {
+      if (account.address === walletAddress) {
+        if (account.config === "") {
+          creatorStatus = 2;
+        } else {
+          creatorStatus = 1;
+        }
+
+        await creator.update({
+          where: {
+            userId: userId,
+          },
+          data: {
+            openSeaUsername: openSeaUsername,
+            walletAddress: walletAddress,
+            openSeaStatus: creatorStatus,
+          },
+        });
+      } else {
+        res.json({
+          statusCode: 3,
+          msg: "Details mismatched",
+        });
+      }
+    } else {
+      res.json({
+        statusCode: 4,
+        msg: "Unable to find the user account",
+      });
+    }
+
+    if (creatorStatus === 1) {
+      res.json({
+        statusCode: 1,
+        msg: "Verified account",
+      });
+    } else if (creatorStatus === 2) {
+      res.json({
+        statusCode: 2,
+        msg: "Valid account",
+      });
+    }
+  };
+});
+
+module.exports = { register, login, creatorVerify, emailCheck };
