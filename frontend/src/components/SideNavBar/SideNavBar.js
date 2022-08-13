a3d183ebb2b530bf5ce4ed023b04a462f934eae5
Janitha Devin Ratnayake <2019cs142@stu.ucsc.lk>
2022-08-13 11:31:33 +0530

    sidebar button click highlight done

--- frontend/src/components/SideNavBar/SideNavBar.js
+++ frontend/src/components/SideNavBar/SideNavBar.js
@@ -1,9 +1,31 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import $ from "jquery";
 
 import "./SideNavBar.css";
 
-function SideNavBar() {
+function SideNavBar({ sideNavBarIndex }) {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    $(".sideNavBarSectionHighlight").each(function (i) {
+      const index = parseInt(sideNavBarIndex);
+
+      if (i === index) {
+        $(this).addClass("activesb");
+      }
+    });
+
+    $(".sideNavBarSectionHighlight").click(function (e) {
+      console.log("Hi");
+      $(".sideNavBarSectionHighlight").each(function (i) {
+        console.log(i);
+        $(this).removeClass("activesb");
+      });
+
+      $(this).addClass("activesb");
+    });
+  }, []);
 
   return (
     <span className="sideNavBar">
@@ -11,7 +33,7 @@ function SideNavBar() {
         <div className="content-top">
           <ul className="list-unstyled">
             <li
-              className="activesb"
+              className="sideNavBarSectionHighlight"
               onClick={() => {
                 navigate("/feed");
               }}
@@ -20,22 +42,23 @@ function SideNavBar() {
                 <i className="bi bi-rss icon-theme"></i>Feed
               </a>
             </li>
-            <li>
+            <li className="sideNavBarSectionHighlight">
               <a>
                 <i className="bi bi-people-fill icon-theme"></i>Find Creators
               </a>
             </li>
-            <li>
+            <li className="sideNavBarSectionHighlight">
               <a>
                 <i className="bi bi-star icon-theme"></i>Favourits
               </a>
             </li>
-            <li>
+            <li className="sideNavBarSectionHighlight">
               <a>
                 <i className="bi bi-chat-dots icon-theme"></i>Chat
               </a>
             </li>
             <li
+              className="sideNavBarSectionHighlight"
               onClick={() => {
                 navigate("/creator/analytics");
               }}
@@ -45,6 +68,7 @@ function SideNavBar() {
               </a>
             </li>
             <li
+              className="sideNavBarSectionHighlight"
               onClick={() => {
                 navigate("/settings");
               }}
@@ -54,6 +78,7 @@ function SideNavBar() {
               </a>
             </li>
             <li
+              className="sideNavBarSectionHighlight"
               onClick={() => {
                 navigate("/Advertisment");
               }}
