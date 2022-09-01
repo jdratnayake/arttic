a59f0863403811eac1a5a6a7ba73bbba7bdaab35
jdratnayake <janithadevin@gmail.com>
2022-09-01 20:52:10 +0530

    post analytics done

--- backend/controllers/creatorAnalyticsController.js
+++ backend/controllers/creatorAnalyticsController.js
@@ -1,8 +1,15 @@
 const { PrismaClient } = require("@prisma/client");
 const asyncHandler = require("express-async-handler");
 
-const { user, transactionLog, advertisement, userReport, userSubscribe } =
-  new PrismaClient();
+const {
+  user,
+  transactionLog,
+  advertisement,
+  userReport,
+  userSubscribe,
+  post,
+  postView,
+} = new PrismaClient();
 
 const round = (value, precision) => {
   var multiplier = Math.pow(10, precision || 0);
@@ -93,6 +100,170 @@ const getFollowerAnalytics = asyncHandler(async (req, res) => {
   res.json(outputData);
 });
 
+const getPostAnalytics = asyncHandler(async (req, res) => {
+  const userId = parseInt(req.headers.userid);
+
+  const previousToday = new Date();
+  previousToday.setDate(previousToday.getDate() - 7);
+
+  // Start
+  const timeList = [];
+  const userCountList = [];
+  const chartDate = new Date();
+  chartDate.setDate(chartDate.getDate() - 7);
+
+  for (let i = 0; i < 7; i++) {
+    chartDate.setDate(chartDate.getDate() + 1);
+    timeList.push(chartDate.getMonth() + 1 + "/" + chartDate.getDate());
+
+    const userCount = await post.aggregate({
+      where: {
+        AND: [
+          {
+            creatorId: userId,
+          },
+          { publishedDate: { lt: chartDate } },
+        ],
+      },
+
+      _count: {
+        postId: true,
+      },
+    });
+
+    userCountList.push(userCount["_count"]["postId"]);
+  }
+
+  const newFollowers = await post.aggregate({
+    where: {
+      creatorId: userId,
+    },
+
+    _count: {
+      postId: true,
+    },
+  });
+
+  const newFollowersPreviousWeek = await post.aggregate({
+    where: {
+      AND: [
+        {
+          creatorId: userId,
+        },
+
+        { publishedDate: { lt: previousToday } },
+      ],
+    },
+
+    _count: {
+      postId: true,
+    },
+  });
+
+  const newFollowersCount = newFollowers["_count"]["postId"];
+  const newFollowersPreviousWeekCount =
+    newFollowersPreviousWeek["_count"]["postId"];
+
+  const newFollowersDetails = [
+    newFollowersCount,
+    round(
+      ((newFollowersCount - newFollowersPreviousWeekCount) /
+        (newFollowersPreviousWeekCount || 1)) *
+        100,
+      2
+    ),
+  ];
+  // End
+
+  const outputData = {
+    newPostDetails: newFollowersDetails,
+    timeList,
+    postCountList: userCountList,
+  };
+
+  res.json(outputData);
+});
+
+const getPostList = asyncHandler(async (req, res) => {
+  const userId = parseInt(req.headers.userid);
+
+  const postDetails = await post.findMany({
+    where: {
+      creatorId: userId,
+    },
+    select: {
+      postId: true,
+      publishedDate: true,
+      imagevideo: true,
+      description: true,
+    },
+  });
+
+  res.json(postDetails);
+});
+
+const getSinglePostAnalytics = asyncHandler(async (req, res) => {
+  const postId = parseInt(req.headers.postid);
+
+  const previousToday = new Date();
+  previousToday.setDate(previousToday.getDate() - 7);
+
+  // Start
+  const timeList = [];
+  const userCountList = [];
+  const chartDate = new Date();
+  chartDate.setDate(chartDate.getDate() - 7);
+
+  for (let i = 0; i < 7; i++) {
+    chartDate.setDate(chartDate.getDate() + 1);
+    timeList.push(chartDate.getMonth() + 1 + "/" + chartDate.getDate());
+
+    const userCount = await postView.aggregate({
+      where: {
+        AND: [
+          {
+            postId,
+          },
+          { postViewedDate: { lt: chartDate } },
+        ],
+      },
+
+      _count: {
+        postViewId: true,
+      },
+    });
+
+    userCountList.push(userCount["_count"]["postViewId"]);
+  }
+
+  // End
+
+  const outputData = {
+    timeList,
+    postCountList: userCountList,
+  };
+
+  res.json(outputData);
+});
+
+const getAdvertismentAnalytics = asyncHandler(async (req, res) => {
+  res.json("Hi");
+});
+
+const getAdvertismentList = asyncHandler(async (req, res) => {
+  res.json("Hi");
+});
+
+const getSingleAdvertismentAnalytics = asyncHandler(async (req, res) => {
+  res.json("Hi");
+});
+
 module.exports = {
   getFollowerAnalytics,
+  getPostAnalytics,
+  getPostList,
+  getSinglePostAnalytics,
+  getAdvertismentAnalytics,
+  getAdvertismentList,
+  getSingleAdvertismentAnalytics,
 };
--- backend/routes/creatorAnalyticsRoutes.js
+++ backend/routes/creatorAnalyticsRoutes.js
@@ -5,11 +5,31 @@ const { validateToken } = require("../middleware/AuthMiddleware");
 // import controllers
 const {
   getFollowerAnalytics,
+  getPostAnalytics,
+  getPostList,
+  getAdvertismentAnalytics,
+  getAdvertismentList,
+  getSinglePostAnalytics,
+  getSingleAdvertismentAnalytics,
 } = require("../controllers/creatorAnalyticsController");
 
 const router = express.Router();
 
 // define specific routes
 router.get("/getfolloweranalytics", validateToken, getFollowerAnalytics);
+router.get("/getpostanalytics", validateToken, getPostAnalytics);
+router.get("/getpostlist", validateToken, getPostList);
+router.get(
+  "/getadvertismentanalytics",
+  validateToken,
+  getAdvertismentAnalytics
+);
+router.get("/getadvertismentlist", validateToken, getAdvertismentList);
+router.get("/getsinglepostanalytics", validateToken, getSinglePostAnalytics);
+router.get(
+  "/getsingleadvertismentanalytics",
+  validateToken,
+  getSingleAdvertismentAnalytics
+);
 
 module.exports = router;
