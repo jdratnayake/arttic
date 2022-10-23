235073d9b793839256b9898ff6614a54bf83c5d2
Lavinka99 <laviweerasekara99@gmail.com>
2022-10-23 16:07:21 +0530

    search bug resolved

--- frontend/src/pages/SearchCreatorList/SearchCreatorList.js
+++ frontend/src/pages/SearchCreatorList/SearchCreatorList.js
@@ -2,75 +2,68 @@ import "./SearchCreatorList.css";
 import CreatorCard from "../../components/CreatorCard/CreatorCard";
 import { useState, useEffect } from "react";
 import {
-    API_URL,
-    PROFILE_PIC_URL,
-    COVER_PIC_URL,
+  API_URL,
+  PROFILE_PIC_URL,
+  COVER_PIC_URL,
 } from "../../constants/globalConstants";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { useLocation, useParams } from "react-router-dom";
 
 function SearchCreatorList() {
+  const local = useLocation();
+  const name = local.state.name;
+  //window.location.reload(false);
+  const [creatorDetails, setCreatorDetails] = useState([]);
 
-    const local = useLocation();
-    const name = local.state.name;
-    //window.location.reload(false);
-    const [creatorDetails, setCreatorDetails] = useState([]);
+  const userInfo = useSelector((state) => state.userInfo);
+  const { userId, accessToken } = userInfo.user;
 
-    const userInfo = useSelector((state) => state.userInfo);
-    const { userId, accessToken } = userInfo.user;
+  //getCreatorDetails();
+  // get creator details -------------------------------------------------
+  const getCreatorDetails = async () => {
+    const head = {
+      headers: {
+        authorization: accessToken,
+        name: name,
+      },
+    };
 
-    //getCreatorDetails();
-    // get creator details -------------------------------------------------
-    const getCreatorDetails = async () => {
-        const head = {
-            headers: {
-                authorization: accessToken,
-                name: name
-            },
-        }
+    console.log(name);
+    await axios
+      .get(API_URL + "/user/getallcreatorsdetails", head)
+      .then((res) => {
+        setCreatorDetails(res.data);
+        console.log(res.data);
+      });
+  };
+  // end creator details --------------------------------------------------
 
-        console.log(name);
-        await axios
-            .get(API_URL + "/user/getallcreatorsdetails", head)
-            .then((res) => {
-                setCreatorDetails(res.data);
-                console.log(res.data);
-            });
-
-
-    }
-    // end creator details --------------------------------------------------
-
-    useEffect(() => {
-        getCreatorDetails();
-    }, []);
-    return (
-
-        <span className="subscribedCreatorsPage">
-            <div className="row row-cols-1 row-cols-md-1 row-cols-md-1">
-
-                {creatorDetails.length == 0
-                    ? <h3 className="mb-0">No result for '{name}'</h3>
-                    : <h3 className="mb-0">Search result for '{name}'</h3>
-                }
-            </div>
-            <div class="row row-cols-1 row-cols-md-2 row-cols-md-3">
-
-                {creatorDetails.map((data) => (
-                    <CreatorCard
-                        userId={data.userId}
-                        coverPhoto={COVER_PIC_URL + data.coverPhoto}
-                        profilePhoto={PROFILE_PIC_URL + data.profilePhoto}
-                        name={data.name}
-                        date={data.joinedDate}
-                    />
-                ))}
-
-            </div>
-        </span>
-
-    )
+  useEffect(() => {
+    getCreatorDetails();
+  }, [name]);
+  return (
+    <span className="subscribedCreatorsPage">
+      <div className="row row-cols-1 row-cols-md-1 row-cols-md-1">
+        {creatorDetails.length == 0 ? (
+          <h3 className="mb-0">No result for '{name}'</h3>
+        ) : (
+          <h3 className="mb-0">Search result for '{name}'</h3>
+        )}
+      </div>
+      <div class="row row-cols-1 row-cols-md-2 row-cols-md-3">
+        {creatorDetails.map((data) => (
+          <CreatorCard
+            userId={data.userId}
+            coverPhoto={COVER_PIC_URL + data.coverPhoto}
+            profilePhoto={PROFILE_PIC_URL + data.profilePhoto}
+            name={data.name}
+            date={data.joinedDate}
+          />
+        ))}
+      </div>
+    </span>
+  );
 }
 
 export default SearchCreatorList;
