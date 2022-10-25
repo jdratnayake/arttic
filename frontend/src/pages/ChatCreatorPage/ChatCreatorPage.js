42dbf3b869bf339a84c0977fb151255f50136b70
jdratnayake <janithadevin@gmail.com>
2022-10-26 02:52:03 +0530

    bug fixed and resources added

--- frontend/src/pages/ChatCreatorPage/ChatCreatorPage.js
+++ frontend/src/pages/ChatCreatorPage/ChatCreatorPage.js
@@ -124,7 +124,7 @@ function ChatCreatorPage() {
                   <h4>Recent</h4>
                 </div>
                 <div class="srch_bar">
-                  <div class="stylish-input-group">
+                  {/* <div class="stylish-input-group">
                     <input
                       type="text"
                       class="search-bar"
@@ -136,7 +136,7 @@ function ChatCreatorPage() {
                         <i class="fa fa-search" aria-hidden="true"></i>{" "}
                       </button>
                     </span>{" "}
-                  </div>
+                  </div> */}
                 </div>
               </div>
               <div class="inbox_chat">
